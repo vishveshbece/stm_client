78077ccf169b4a4d9c014343d5f28b9192329f28
vishvesh B <vishveshbece@gmail.com>
2026-02-21 21:58:31 +0530

    last backend bug clearance

--- backend/server.js
+++ backend/server.js
@@ -38,5 +38,5 @@ app.use('/api/registrations', require('./routes/registrations'));
 app.use('/api/admin', require('./routes/admin'));
 app.use('/api/attendance', require('./routes/attendance'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
